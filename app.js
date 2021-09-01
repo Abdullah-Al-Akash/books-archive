2d9c9508e89223009bf522ce4d3ad906bfcd5fd6
Abdullah Al Akash <aabdullahalakash@gmail.com>
2021-09-02 04:10:16 +0600

    Update Error Message

--- app.js
+++ app.js
@@ -1,21 +1,45 @@
+const totalResult = document.getElementById('total-result');
+const booksContainer = document.getElementById('books-container');
+const errorContainer = document.getElementById('error');
+
 // Search Book:
 document.getElementById('search-btn').addEventListener('click', function () {
+        // Clear Old Data
+        totalResult.textContent = '';
+        booksContainer.textContent = '';
+        errorContainer.textContent = '';
+
         const searchInput = document.getElementById('search-input');
         const searchText = searchInput.value;
 
         // Fetch Data:
         fetch(`https://openlibrary.org/search.json?q=${searchText}`)
                 .then(res => res.json())
-                .then(data => displayBooks(data.docs));
+                .then(data => displayBooks(data.docs.slice(0, 10)));
         searchInput.value = '';
 })
 
+// Display Books:
 const displayBooks = (books) => {
-        // console.log(books);
-        const booksContainer = document.getElementById('books-container');
+
+        const h6 = document.createElement('h6')
+        h6.innerText = 'found ' + books.length + ' books';
+        totalResult.appendChild(h6);
+
+        // Error Message for didn't find any book:
+        if (books.length === 0) {
+                totalResult.classList.add('d-none');
+                const div = document.createElement('div');
+                div.innerHTML = `
+                        <h4 class="text-center text-danger mt-5">No Book Found...</h4>
+                `;
+                errorContainer.appendChild(div);
+        }
+
+        // use forEach for every single book:
         books.forEach((book) => {
                 console.log(book);
-                const div = document.createElement('div');
+                const div = document.createElement('div')
                 div.innerHTML = `
                         <div class="col book">
                                 <div class="card h-100">
