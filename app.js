41ec73dc13e0a8e2515261559061b3fdf027af06
Abdullah Al Akash <aabdullahalakash@gmail.com>
2021-09-02 16:57:33 +0600

    Update Spinner And Proverbs

--- app.js
+++ app.js
@@ -2,12 +2,19 @@ const totalResult = document.getElementById('total-result');
 const booksContainer = document.getElementById('books-container');
 const errorContainer = document.getElementById('error');
 
+// Title Proverb:
+const titleProverb = titleDisplay => {
+        document.getElementById('title').style.display = titleDisplay;
+}
+
 // Spinner Loading:
 const toggleSpinner = displayStyle => {
-        document.getElementById('spinner').classList.add(displayStyle);
+        document.getElementById('spinner').style.display = displayStyle;
 }
 // Search Book:
 document.getElementById('search-btn').addEventListener('click', function () {
+        toggleSpinner('block');
+        titleProverb('none');
         // Clear Old Data
         totalResult.textContent = '';
         booksContainer.textContent = '';
@@ -18,7 +25,7 @@ document.getElementById('search-btn').addEventListener('click', function () {
         // Fetch Data:
         fetch(`https://openlibrary.org/search.json?q=${searchText}`)
                 .then(res => res.json())
-                .then(data => displayBooks(data.docs.slice(0, 10)));
+                .then(data => displayBooks(data.docs));
         searchInput.value = '';
 })
 
@@ -41,23 +48,25 @@ const displayBooks = (books) => {
 
         // use forEach for every single book:
         books.forEach((book) => {
-                console.log(book);
                 const div = document.createElement('div')
                 div.innerHTML = `
                         <div class="col book">
                                 <div class="card h-100">
-                                        <div class="p-5">
-                                                <img style="height:400px" src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="img-fluid border border-dark border-3" alt="...">
+                                        <div class="pt-4 ps-4 pe-4">
+                                                <img style="height:400px" src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="img-fluid border border-dark border-3 w-100" alt="...">
                                         </div>
-                                        <div class="card-body">
-                                                <h5 class="card-title text-success">${book.title}</h5>
-                                                <h5 class="card-title">Author's Name:${book.author_name}</h5>
+                                        <div class="card-body" style="height: 150px; overflow: hidden">
+                                                <h5 class="card-title text-success"> ${book.title}</h5>
+                                                <h5 class="card-title">Author's Name: ${book.author_name}</h5>
                                                 <p class="card-text">Publishers: ${book.publisher}</p>
-                                                <p class="card-text">First Published: ${book.first_publish_year}</p>
+                                        </div>
+                                        <div class="card-footer">
+                                                <small class="text-success">First Published: ${book.first_publish_year}</small>
                                         </div>
                                 </div>
                         </div>
                 `;
                 booksContainer.appendChild(div);
         })
+        toggleSpinner('none')
 }
